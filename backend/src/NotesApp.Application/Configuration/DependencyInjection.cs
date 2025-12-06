32ab69100a37c376ebfa90b14f47b4a36e38fab4
Elvis Knapman <46550724+ElvisKnapman@users.noreply.github.com>
2025-12-06 14:10:46 -0500

    Extract DI registration to extension methods

---
+++ backend/src/NotesApp.Application/Configuration/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NotesApp.Application.Interfaces;
+using NotesApp.Application.Services;
+
+namespace NotesApp.Application.Configuration;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // Register everything for Application layer here
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IAuthService, AuthService>();
+
+        return services;
+    }
+}
---
+++ backend/src/NotesApp.Infrastructure/Configuration/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NotesApp.Application.Interfaces;
+using NotesApp.Domain.Entities;
+using NotesApp.Infrastructure.Persistence;
+using NotesApp.Infrastructure.Repositories;
+
+namespace NotesApp.Infrastructure.Configuration;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+    {
+        // Register everything for Infrastructure layer here
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddTransient<IPasswordHasher<User>, PasswordHasher<User>>();
+
+        return services;
+    }
+}
