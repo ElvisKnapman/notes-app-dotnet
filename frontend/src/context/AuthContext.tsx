7581e31a1d88931daac26a33c31ec9e11c78586d
Elvis Knapman <46550724+ElvisKnapman@users.noreply.github.com>
2025-12-17 23:00:44 -0500

    Scaffold AuthContext store

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, useState } from 'react';
+
+interface AuthContextValue {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextValue | undefined>(undefined);
+
+export function AuthContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  function login() {
+    setIsAuthenticated(true);
+  }
+
+  function logout() {
+    setIsAuthenticated(false);
+  }
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
