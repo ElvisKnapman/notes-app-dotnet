dcd7f537a9519c1b767d1876c6297245dc3e240b
Elvis Knapman <46550724+ElvisKnapman@users.noreply.github.com>
2025-12-29 19:52:06 -0500

    change NotesListPage to fetch notes directly
    - note context removed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,12 @@
 import { BrowserRouter } from 'react-router';
 import AppRoutes from './routes/AppRoutes';
 import { AuthProvider } from './context/AuthContext';
-import { NoteProvider } from './context/NoteContext';
-// import NotesPage from './pages/NotesPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <NoteProvider>
-          <AppRoutes />
-        </NoteProvider>
+        <AppRoutes />
       </AuthProvider>
     </BrowserRouter>
   );
--- frontend/src/pages/NotesListPage.tsx
+++ frontend/src/pages/NotesListPage.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 import NotesList from '../components/Notes/NoteList/NotesList';
-import { useNotes } from '../context/useNotes';
-import type { GetNotesParams } from '../api/noteService';
+import { getNotes, type GetNotesParams } from '../api/noteService';
+import { ApiError } from '../errors/ApiError';
+import type { Note } from '../models/notes/Notes';
 
 export default function NotesListPage() {
-  const { notes, isLoading, errorMessage, loadNotes } = useNotes();
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [query, setQuery] = useState<GetNotesParams>({
     pageSize: 10,
     pageNumber: 1,
@@ -14,13 +17,31 @@ export default function NotesListPage() {
   });
 
   useEffect(() => {
+    async function loadNotes() {
+      setIsLoading(true);
+      setErrorMessage(null);
+
+      try {
+        const data = await getNotes(query);
+        setNotes(data.items);
+      } catch (error) {
+        if (error instanceof ApiError) {
+          setErrorMessage(error.message);
+        } else {
+          setErrorMessage('Unknown error occurred fetching notes.');
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
     loadNotes();
   }, []);
 
   return (
     <>
       <h2>Notes Page</h2>
-      {errorMessage && <p>{errorMessage}</p>}
+      {!isLoading && errorMessage !== null && <p>{errorMessage}</p>}
       {isLoading ? <p>Loading notes...</p> : <NotesList notes={notes} />}
     </>
   );
